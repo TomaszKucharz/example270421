1dec4a727928f995c212efc28ce49dc872761bbd
TomaszKucharz <tkucharz.mail@gmail.com>
2021-09-03 13:54:37 +0200

    start new js app

---
+++ main.js
@@ -0,0 +1,8 @@
+const clickBanner = document.querySelector(`.fas`);
+
+clickBanner.addEventListener(`click`, () =>
+{
+   // console.log("klikam w FAS");
+   const menu = document.querySelector(`.menu`);
+
+})
