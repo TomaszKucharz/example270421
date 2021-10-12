a09e86b0cc2764aa682c57dd4b250116b3109d58
TomaszKucharz <tkucharz.mail@gmail.com>
2021-10-12 21:06:48 +0200

    aktualizacja 10.10.2021

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const clickBanner = document.querySelector(`.fas`);
+const arrow = document.querySelector(`.arrow`);
+const menuM = document.querySelector(`.menu`);
+const dark = document.querySelector(`dark`);
 
 clickBanner.addEventListener(`click`, () =>
 {
-   // console.log("klikam w FAS");
-   const menu = document.querySelector(`.menu`);
+   console.log("klikam w FAS");
+   menuM.style.height = `300px`;
+   menuM.style.transition = `0.5s`;
+   clickBanner.classList.toggle(`on`);
+   // clickBanner.style.transition = `0.5s`;
 
 })
